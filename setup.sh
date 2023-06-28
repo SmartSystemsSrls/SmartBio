3bb18854edc9f03df99586f801fa4619d721fa3c
savish28 <32800267+savish28@users.noreply.github.com>
2023-06-28 17:31:55 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,5 +8,6 @@ sudo apt-get install python3-venv -y
 python3 -m ven env
 source env/bin/activaye
 pip3 install deepface
+pip3 install flask
 tmux new-session -d -s face 'source env/bin/activate && python main.py'
 tmux new-session -d -s ui 'source env/bin/activate && python app.py'
