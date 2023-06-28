ecbfc33521f0b3280afdf6f2a00ce0c3d45b1bad
savish28 <32800267+savish28@users.noreply.github.com>
2023-06-28 17:33:21 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo apt-get update -y
 sudo apt install python3-pip -y
 sudo apt-get install ffmpeg libsm6 libxext6  -y
 sudo apt-get install python3-venv -y
-python3 -m ven env
+python3 -m venv env
 source env/bin/activaye
 pip3 install deepface
 pip3 install flask
