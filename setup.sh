fbf56ec5e8954f5473b582fb53115488e92b6573
savish28 <32800267+savish28@users.noreply.github.com>
2023-06-28 17:37:40 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ sudo apt install python3-pip -y
 sudo apt-get install ffmpeg libsm6 libxext6  -y
 sudo apt-get install python3-venv -y
 python3 -m venv env
-source env/bin/activaye
+source env/bin/activate
 pip3 install deepface
 pip3 install flask
 tmux new-session -d -s face 'source env/bin/activate && python main.py'
